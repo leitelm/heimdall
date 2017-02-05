faf5f38d93cfa7c9b076cee4e14978f988025ff5
Brunno Araujo <brunnobaraujo@gmail.com>
2017-02-05 15:04:39 -0300

    Add serial

--- Arduino/src/Serial.ino
+++ Arduino/src/Serial.ino
@@ -1,12 +1,45 @@
+char serialdata = "";
+
 void serial_setup(){
   Serial.begin(115200);
 }
 
-char serial_loop(){
-  char serialData = 0;
-  if (Serial.available() > 0){
-    serialData = Serial.read();
-    Serial.println(serialData);
+void serial_loop(){
+  while(Serial.available() > 0){
+    serialdata = Serial.read();
   }
-  return serialData;
+
+  switch(serialdata){
+    case 'f':{
+      velocity1 = 200;
+      velocity2 = 200;
+      Serial.println('f');
+      break;
+    }
+    case 'r':{
+      velocity1 = -200;
+      velocity2 = -200;
+      Serial.println('r');
+      break;
+    }
+    case 's':{
+      velocity1 = 0;
+      velocity2 = 0;
+      Serial.println('s');
+      break;
+    }
+    case '+':{
+      velocity1 = velocity1 + 50;
+      velocity2 = velocity2 +50;
+      Serial.println('+');
+      break;
+    }
+    case '-':{
+      velocity1 = velocity1 - 50;
+      velocity2 = velocity2 - 50;
+      Serial.println('-');
+      break;
+    }
+  }
+  serialdata = "";
 }
--- Raspberry/myserial.py
+++ Raspberry/myserial.py
@@ -1,7 +1,21 @@
-import serial
+# -*- coding: utf-8 -*-
 
-myserial = serial.Serial('/dev/ttyACM0', 115200)
+import serial, time
 
-while True:
-	valor = raw_input()
-	myserial.write(valor)
+arduino = serial.Serial('/dev/ttyACM0', 115200)
+
+# while True:
+# 	valor = raw_input()
+# 	myserial.write(valor)
+
+fwd = 'f'
+reverse = 'r'
+stop = 's'
+
+arduino.write(fwd)
+time.sleep(5)
+arduino.write(reverse)
+time.sleep(5)
+arduino.write(stop)
+time.sleep(5)
+arduino.write(fwd)
