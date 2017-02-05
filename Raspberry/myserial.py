500eaaa3e01292fb9233b8fa24604d52a13c7064
Brunno Araujo <brunnobaraujo@gmail.com>
2017-02-05 15:57:34 -0300

    Add xbox controller

--- Raspberry/myserial.py
+++ Raspberry/myserial.py
@@ -1,21 +1,75 @@
 # -*- coding: utf-8 -*-
+import serial, time, xbox
 
-import serial, time
+def fmtFloat(n):
+    return '{:6.3f}'.format(n)
+    
+joy = xbox.Joystick()
 
-arduino = serial.Serial('/dev/ttyACM0', 115200)
+# arduino = serial.Serial('/dev/ttyACM0', 115200)
 
-# while True:
-# 	valor = raw_input()
-# 	myserial.write(valor)
+print "starting heimdall app"
 
-fwd = 'f'
-reverse = 'r'
-stop = 's'
+while not joy.Back():
+    # Show connection status
+    if joy.connected():
+        print "Connected   ",
+    else:
+        print "Disconnected",
+    print 'X:', joy.rightX()
+    print 'Y:', joy.rightY()
 
-arduino.write(fwd)
-time.sleep(5)
-arduino.write(reverse)
-time.sleep(5)
-arduino.write(stop)
-time.sleep(5)
-arduino.write(fwd)
+    if(joy.rightY() > 0):
+    	# arduino.write('f')
+    	pass
+    if(joy.rightY() == 0):
+    	# arduino.write('s')
+    	pass
+    if(joy.rightY() < 0):
+    	# arduino.write('r')
+    	pass
+
+joy.close()
+
+    # # Left analog stick
+    # print "Lx,Ly ",fmtFloat(joy.leftX()),fmtFloat(joy.leftY()),
+    # print "Rx,Ry ",fmtFloat(joy.rightX()),fmtFloat(joy.rightY()),
+    # # Right trigger
+    # print "Rtrg ",fmtFloat(joy.rightTrigger()),
+    # # A/B/X/Y buttons
+    # print "Buttons ",
+    # if joy.A():
+    #     print "A",
+    # else:
+    #     print " ",
+    # if joy.B():
+    #     print "B",
+    # else:
+    #     print " ",
+    # if joy.X():
+    #     print "X",
+    # else:
+    #     print " ",
+    # if joy.Y():
+    #     print "Y",
+    # else:
+    #     print " ",
+    # # Dpad U/D/L/R
+    # print "Dpad ",
+    # if joy.dpadUp():
+    #     print "U",
+    # else:
+    #     print " ",
+    # if joy.dpadDown():
+    #     print "D",
+    # else:
+    #     print " ",
+    # if joy.dpadLeft():
+    #     print "L",
+    # else:
+    #     print " ",
+    # if joy.dpadRight():
+    #     print "R",
+    # else:
+    #     print " ",
+        
